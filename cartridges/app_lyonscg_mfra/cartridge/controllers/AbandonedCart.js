5e4c3e9e0c289cf9b3541f205e48909fa0c921c1
Dave Haas <dhaas@lyonscg.com>
2018-09-28 13:47:48 -0500

    LRA-103 fixing linting errors

--- cartridges/app_lyonscg_mfra/cartridge/controllers/AbandonedCart.js
+++ cartridges/app_lyonscg_mfra/cartridge/controllers/AbandonedCart.js
@@ -3,7 +3,7 @@
  * Please see the Start route definition below for more detail.
  * Note: Depends on base64Url utility module.
  */
-'use strict'
+'use strict';
 
 var server = require('server');
 var BasketMgr = require('dw/order/BasketMgr');
@@ -11,9 +11,10 @@ var URLUtils = require('dw/web/URLUtils');
 
 /**
  * Checks if the customer basket exists and has any product line items.
+ * @returns {boolean} does the customer have a non empty basket
  */
 function hasNonEmptyBasket() {
-    let basket = BasketMgr.getCurrentBasket();
+    var basket = BasketMgr.getCurrentBasket();
 
     // Is there an existing basket?
     if (basket) {
@@ -28,15 +29,17 @@ function hasNonEmptyBasket() {
 
 /**
  * Parses basket info from query param into simple JSON basket info object.
+ * @param {string} querystring from the url
+ * @returns {Object} object data representing what needs to be added to basket
  */
 function parseQueryParams(querystring) {
-    let basketInfo = {};
+    var basketInfo = {};
 
     // Basket specified as base64Url encoded JSON.
     if (querystring.basket) {
-        let base64Url = require('base64Url');
-        let decoded = base64Url.decode(querystring.basket);
-        let newBasket = JSON.parse(decoded);
+        var base64Url = require('base64Url');
+        var decoded = base64Url.decode(querystring.basket);
+        var newBasket = JSON.parse(decoded);
 
         if (newBasket && newBasket.products && newBasket.products.length) {
             basketInfo = newBasket;
@@ -45,15 +48,13 @@ function parseQueryParams(querystring) {
 
     // Fallback to basket specified as simple indexed parameters.
     if (!basketInfo.products && querystring.p) {
-        let index = 0;
-        let paramIndex = '';
-        
+        var index = 0;
+        var paramIndex = '';
         basketInfo.products = [];
-
-        while(querystring['p' + paramIndex]) {
-            basketInfo.products.push({ 
+        while (querystring['p' + paramIndex]) {
+            basketInfo.products.push({
                 pid: querystring['p' + paramIndex],
-                quantity: parseInt(querystring['q' + paramIndex] || 1)
+                quantity: parseInt(querystring['q' + paramIndex] || 1, 10)
             });
 
             paramIndex = ++index + '';
@@ -67,17 +68,18 @@ function parseQueryParams(querystring) {
  * Takes simple basket info and attempts to add each requested product to the cart.
  * Using existing basket helper scripts from base refarch for consistency.
  * Logs all errors but does not throw. May result in empty cart, but not an error page.
+ * @param {Object} basketInfo - object notation with product Ids and quantities to add to the basket
  */
 function createBasket(basketInfo) {
-    let Transaction = require('dw/system/Transaction');
-    let ProductMgr = require('dw/catalog/ProductMgr');
-    let Logger = require('dw/system/Logger');
-    let cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
-    let basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
-    let currentbasket = BasketMgr.getCurrentOrNewBasket();
+    var Transaction = require('dw/system/Transaction');
+    var ProductMgr = require('dw/catalog/ProductMgr');
+    var Logger = require('dw/system/Logger');
+    var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
+    var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+    var currentbasket = BasketMgr.getCurrentOrNewBasket();
 
     if (basketInfo && basketInfo.products && basketInfo.products.length) {
-        let results;
+        var results;
 
         Transaction.begin();
 
@@ -103,10 +105,8 @@ function createBasket(basketInfo) {
             // Do the basket things
             cartHelper.ensureAllShipmentsHaveMethods(currentbasket);
             basketCalculationHelpers.calculateTotals(currentbasket);
-        
             Transaction.commit();
-        } 
-        catch (error) {
+        } catch (error) {
             Transaction.rollback();
 
             // Capture and log the error.
@@ -114,19 +114,19 @@ function createBasket(basketInfo) {
         }
 
         // Individual product errors don't stop cart build, but should be logged.
-        let errors = results.filter(function (result) { return result.error; })
-        .map(function(result) { return result.message; })
+        var errors = results.filter(function (result) { return result.error; })
+        .map(function (result) { return result.message; })
         .join(' | ');
-        
+
         if (errors) {
             Logger.warn('Could not add one or more products to the cart. ' + errors);
-        }        
+        }
     }
 }
 
 /**
  * Navigate to an existing basket, if found. Otherwise create a basket per query params.
- * Either a basket info JSON object is provided as a base64Url encoded basket param, 
+ * Either a basket info JSON object is provided as a base64Url encoded basket param,
  * or indexed p and q query parameters are expected with each ID and quantity.
  * @example Basket param: ?basket=eyJwcm9kdWN0cyI6W3sicGlkIjoic29ueS1jeWJlcnNob3QtZHNjLWg1MCIsInF1YW50aXR5IjoxLCJvcHRpb25zIjpbeyJvcHRpb25JZCI6ImRpZ2l0YWxDYW1lcmFXYXJyYW50eSIsInNlbGVjdGVkVmFsdWVJZCI6IjAwMiJ9XX0seyJwaWQiOiJzb255LXBzMy1idW5kbGUiLCJxdWFudGl0eSI6MSwiY2hpbGRQcm9kdWN0cyI6W3sicGlkIjoiZWFzcG9ydHMtbmFzY2FyLTA5LXBzMyIsInF1YW50aXR5IjoxfSx7InBpZCI6ImVhc3BvcnRzLW1vbm9wb2x5LXBzMyIsInF1YW50aXR5IjoxfSx7InBpZCI6Im5hbWNvLWV0ZXJuYWwtc29uYXRhLXBzMyIsInF1YW50aXR5IjoxfSx7InBpZCI6InNvbnktd2FyaGF3ay1wczMiLCJxdWFudGl0eSI6MX1dfV19
  * @example Sample basket decoded: {
@@ -177,7 +177,7 @@ server.get('Start', function (req, res, next) {
 },
 function (req, res, next) {
     // Parse basket info from the GET request.
-    let basketInfo = parseQueryParams(req.querystring);
+    var basketInfo = parseQueryParams(req.querystring);
     // Attempt to create basket--no error will be thrown!
     createBasket(basketInfo);
 
