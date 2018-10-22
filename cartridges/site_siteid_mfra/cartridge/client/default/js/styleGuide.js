95f0006a7f40d5048fabf80ad60fd6e9351fe287
Kenneth Hurh <khurh@lyonscg.com>
2018-10-22 16:56:16 -0500

    LRA-141 #time 1h 30min #comment Refactored side nav bar to be lists rather than anchor tags; Hid the header and footer for only styleguide; Refactored jQuery to adjust to the current viewport instead of the fixed height

--- cartridges/site_siteid_mfra/cartridge/client/default/js/styleGuide.js
+++ cartridges/site_siteid_mfra/cartridge/client/default/js/styleGuide.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     var sidenav = $('.sg-sidenav');
     var body = $('html, body');
+    var currentWidth = $(window).width();
+    var mobileView = 576;
+
+    $(window).resize(function () {
+        location.reload();
+    });
 
     $('.sg-sidenav a[href*=\\#]').on('click', function (event) {
         event.preventDefault();
-        if (sidenav.height() === 200) {
+        if (currentWidth < mobileView) {
             body.animate({ scrollTop: $(this.hash).offset().top - sidenav.height() }, 500);
         } else {
             body.animate({ scrollTop: $(this.hash).offset().top }, 500);
