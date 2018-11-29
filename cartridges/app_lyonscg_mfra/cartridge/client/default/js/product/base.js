3361da1adbbf435aaa8350c3acf4f4db60c2cada
dkezlya <dkezlya@lyonscg.com>
2018-11-29 15:19:34 +0200

    LRA-145 Implemented zoom functionality on PDP

--- cartridges/app_lyonscg_mfra/cartridge/client/default/js/product/base.js
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/product/base.js
@@ -4,11 +4,38 @@ var base = require('base/product/base');
 var slickConfigs = require('../config/slickConfigs');
 
 /**
+ * Disable PDP Zoom
+ */
+function disableZoom() {
+    $('.slide-link').trigger('zoom.destroy');
+}
+
+/**
+ * Init PDP Zoom
+ */
+function initZoom() {
+    disableZoom();
+
+    var zoomMediaQuery = matchMedia('(min-width: 960px)');
+    var $image = $('.slick-active .slide-link.zoom-enabled');
+    var url = $image.attr('href');
+
+    if ($image.length > 0 && url && url !== 'null' && zoomMediaQuery.matches) {
+        $image.zoom({
+            url: url
+        });
+    }
+}
+
+/**
  * Init the product carousel using a predefined slick configuration
  */
 function carouselInit() {
-    if ($('.product-carousel').length) {
-        $('.product-carousel').not('.slick-initialized').slick(slickConfigs.pdp);
+    var $carousel = $('.product-carousel');
+
+    if ($carousel.length) {
+        $carousel.on('afterChange', initZoom);
+        $carousel.not('.slick-initialized').slick(slickConfigs.pdp);
     }
 }
 
@@ -16,14 +43,18 @@ function carouselInit() {
  * Deconstruct (unslick) the carousel, removing classes and handlers added on slick initialize.
  */
 function carouselUnslick() {
-    if ($('.product-carousel').length && $('.product-carousel').hasClass('slick-initialized')) {
-        $('.product-carousel').slick('unslick');
+    var $carousel = $('.product-carousel');
+
+    if ($carousel.length && $carousel.hasClass('slick-initialized')) {
+        $carousel.off('afterChange', initZoom);
+        $carousel.slick('unslick');
     }
 }
 
 var exportBase = $.extend({}, base, {
     carouselInit: carouselInit,
-    carouselUnslick: carouselUnslick
+    carouselUnslick: carouselUnslick,
+    zoomInit: initZoom
 });
 
 module.exports = exportBase;
--- cartridges/app_lyonscg_mfra/cartridge/client/default/js/product/detail.js
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/product/detail.js
@@ -3,9 +3,40 @@
 var base = require('./base');
 var detail = require('base/product/detail');
 
+/**
+ * Update Main Image section
+ * @param {json} - Product json
+ */
+function updateMainImages(product) {
+    var images = product.images;
+    var imagesZoom = product.imagesZoom['hi-res'];
+    var htmlString = `<!-- Product Image slides -->`;
+
+    images.large.forEach(function (image, idx) {
+        var zoomImage = imagesZoom[idx] ? imagesZoom[idx] : image;
+        var zoomClass = imagesZoom[idx] ? 'zoom-enabled' : 'zoom-disabled';
+
+        var htmlSlide = `<div class="slide">
+            <a href="${zoomImage.url}" class="slide-link ${zoomClass}" title="${zoomImage.title}">
+                <img src="${image.url}" class="slide-img" alt="${image.alt}" />
+            </a>
+        </div>`;
+
+        htmlString += htmlSlide;
+    });
+
+    $('.product-carousel').html(htmlString);
+}
+
 var exportDetail = $.extend({}, detail, {
     updateAttribute: function () {
         base.carouselInit();
+        base.zoomInit();
+
+        $('body').on('product:beforeAttributeSelect', function () {
+            // Unslick the existing images to prepare them for direct js manipulation
+            base.carouselUnslick();
+        });
 
         $('body').on('product:afterAttributeSelect', function (e, response) {
             if ($('.product-detail>.bundle-items').length) {
@@ -18,6 +49,12 @@ var exportDetail = $.extend({}, detail, {
                 $('.product-id').text(response.data.product.id);
                 $('.product-detail:not(".bundle-item")').data('pid', response.data.product.id);
             }
+
+            updateMainImages(response.data.product);
+
+            // Init slick and zoom
+            base.carouselInit();
+            base.zoomInit();
         });
     }
 });
