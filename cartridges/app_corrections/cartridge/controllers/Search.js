d9e7cb82fdac447fee710fd296a5a5ff52e557ba
dkezlya <dkezlya@lyonscg.com>
2018-12-10 15:01:18 +0200

    LRA-189 #comment Implemented category and search pages slot functionality

---
+++ cartridges/app_corrections/cartridge/controllers/Search.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var page = module.superModule;
+var server = require('server');
+
+server.extend(page);
+
+/**
+ * Fixed category rendering template functionality
+ * and added cagory model to the viewData for category type content slot
+ */
+server.append('Show', function (req, res, next) {
+    var ProductSearchModel = require('dw/catalog/ProductSearchModel');
+    var searchHelper = require('*/cartridge/scripts/helpers/searchHelpers');
+    var isAjax = Object.hasOwnProperty.call(req.httpHeaders, 'x-requested-with')
+        && req.httpHeaders['x-requested-with'] === 'XMLHttpRequest';
+    var resultsTemplate = isAjax ? 'search/searchResultsNoDecorator' : 'search/searchResults';
+    var apiProductSearch = new ProductSearchModel();
+    apiProductSearch = searchHelper.setupSearch(apiProductSearch, req.querystring);
+    var categoryTemplate = '';
+
+    if (apiProductSearch.category) {
+        categoryTemplate = apiProductSearch.category.template ? apiProductSearch.category.template : '';
+        var viewData = res.getViewData();
+        viewData.category = apiProductSearch.category;
+        res.setViewData(viewData);
+    }
+
+    if (
+        apiProductSearch.categorySearch
+        && !apiProductSearch.refinedCategorySearch
+        && categoryTemplate
+    ) {
+        if (isAjax) {
+            res.render(resultsTemplate);
+        } else {
+            res.render(categoryTemplate);
+        }
+    } else {
+        res.render(resultsTemplate);
+    }
+
+    return next();
+});
+
+module.exports = server.exports();
