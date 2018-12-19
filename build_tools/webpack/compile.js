506f9b6a919283e8bf6a88ddaf068258b3c0d3db
Davis <ddevries@lyonscg.com>
2018-12-19 00:35:49 -0600

    LRA-163 refactor return of configuration objects to webpack cli

--- build_tools/webpack/compile.js
+++ build_tools/webpack/compile.js
@@ -2,18 +2,18 @@
 'use strict';
 
 const path = require('path');
+const chalk = require('chalk');
 
 module.exports = ((env, argv) => {
-    const WebpackConfiguration = require('./webpack.config.js');
+    const WebpackConfigurator = require('./webpack.config.js');
     const packageFile = require(path.join(process.cwd(), './package.json'));
     const options = (argv.mode === 'development') ? packageFile.buildEnvironment.development :
                                                     packageFile.buildEnvironment.production
-
-    const configurations = new Array();                                            
-    packageFile.sites.forEach((site) => {
-        const siteConfiguration = new WebpackConfiguration(site, options);
-        configurations.push(siteConfiguration.createJsConfiguration());
-        configurations.push(siteConfiguration.createSassConfiguration());
-    });
-    return configurations;
+    
+    console.log(chalk.white(`Initializing compiler in ${chalk.yellow(argv.mode)} mode\n`));
+    return packageFile.sites.reduce((configurations, site) => {
+        let configurator = new WebpackConfigurator(site, options);
+        configurations.push(...configurator.create());
+        return configurations;
+    }, [])
 });
