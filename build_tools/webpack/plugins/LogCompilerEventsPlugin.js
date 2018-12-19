d2a6e30f211da2c124648524939261c7d183e39b
Davis <ddevries@lyonscg.com>
2018-12-19 00:34:32 -0600

    LRA-163 consistent logging for start, success, and watch of compiler

--- build_tools/webpack/plugins/LogCompilerEventsPlugin.js
+++ build_tools/webpack/plugins/LogCompilerEventsPlugin.js
@@ -1,26 +1,24 @@
 'use strict';
 
 const chalk = require('chalk');
-
 module.exports = class LogCompilerEventsPlugin {
     constructor(options) {
         this.cartridges = options.cartridges || [];
         this.siteCartridge = this.cartridges[this.cartridges.findIndex(cartridge => cartridge.alias === 'site')];
-        this.mode = (options.mode).charAt(0).toUpperCase() + (options.mode).slice(1);
+        this.type = (options.type).toUpperCase()
     }
     apply(compiler) {
       compiler.hooks.thisCompilation.tap('LogCompilerEventsPlugin', (compilation) => {
         this.timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-        console.log(chalk.white(`[${this.timestamp}] ${this.mode} build ${chalk.yellow(`starting`)} for site "${chalk.bold(this.siteCartridge.name)}"`));
+        console.log(chalk.white(`[${this.timestamp}] "${chalk.bold(this.siteCartridge.name)}" ${this.type} compiler is ${chalk.yellow(`running`)}`));
+
       })
       compiler.hooks.done.tap('LogCompilerEventsPlugin', (stats) => {
-        // if verbose
-            // list all emitted assets
-            // console.log(chalk.white(`\tBuild time: ${((stats.endTime - stats.startTime)/1000).toFixed(2)} seconds`));
-        console.log(chalk.white(`[${this.timestamp}] ${this.mode} build ${chalk.green(`successful`)} for site "${chalk.bold(this.siteCartridge.name)}"\n`));
-        if (this.watch) {
-            console.log(`Compiler for site "${this.siteCartridge.name}" is ${chalk.cyan(`ready`)} for changes\n`);
-        }
+            const timing = ((stats.endTime - stats.startTime)/1000).toFixed(2);
+            console.log(chalk.white(`[${this.timestamp}] "${chalk.bold(this.siteCartridge.name)}" ${this.type} compiler was ${chalk.green(`successful`)} in ${timing} seconds`));
+            if (this.watch) {
+                console.log(chalk.white(`[${this.timestamp}] "${chalk.bold(this.siteCartridge.name)}" ${this.type} compiler is ${chalk.cyan(`ready`)} for changes`));
+            }
       });
       compiler.hooks.watchRun.tap('LogCompilerEventsPlugin', (compiler) => {
           this.watch = true;
