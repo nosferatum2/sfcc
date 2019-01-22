1c39a089eb7737cb0723e248e14b9696840ee50e
Davis <ddevries@lyonscg.com>
2019-01-22 17:39:28 -0600

    LRA-229 clear validation errors on field change

---
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/components/clientSideValidation.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var baseClientSideValidation = require('base/components/clientSideValidation');
+
+module.exports = $.extend(baseClientSideValidation, {
+    /**
+     * Clears the validation errors of an invalidated form element after a change occurs
+     */
+    clearErrorsOnChange: function () {
+        $(':input, .form-control').on('change', function () {
+            if ($(this).hasClass('is-invalid')) {
+                $(this).removeClass('is-invalid');
+                $(this).closest('.invalid-feedback').remove();
+            }
+        });
+    }
+});
