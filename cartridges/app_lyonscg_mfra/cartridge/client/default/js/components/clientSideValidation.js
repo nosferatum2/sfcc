3c30936a58498664a8efb014f5d058792b91acbd
Davis <ddevries@lyonscg.com>
2019-01-28 08:53:48 -0600

    LRA-229 clear errors on keydown

--- cartridges/app_lyonscg_mfra/cartridge/client/default/js/components/clientSideValidation.js
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/components/clientSideValidation.js
@@ -7,7 +7,7 @@ module.exports = $.extend(baseClientSideValidation, {
      * Clears the validation errors of an invalidated form element after a change occurs
      */
     clearErrorsOnChange: function () {
-        $(':input, .form-control').on('change', function () {
+        $(':input, .form-control').on('change, keydown', function () {
             if ($(this).hasClass('is-invalid')) {
                 $(this).removeClass('is-invalid');
                 $(this).closest('.invalid-feedback').remove();
