feb95a97fdc521181bf5c8c90122d47e2b0f7f61
Eric Fung <eric.fung@capgemini.com>
2019-02-25 21:52:24 +0000

    Merged in feature/LRA-198-manual-recommendations (pull request #133)
    LRA-198: Manual Recommendations Template
    
    * LRA-198 #comment Added Manual Recommendations templates #time 30m
    
    * LRA-198 #comment Added include for modules, global resources & null product bug fix if not available and updated resource naming based on rec type number
    
    * LRA-198 #comment added mobile product tile slider configs and styling updates
    
    Approved-by: Jen Scott <jen.scott@capgemini.com>
    Approved-by: Kent Winfield <kwinfield@lyonscg.com>
    Approved-by: Dave Haas <dhaas@lyonscg.com>

--- cartridges/app_lyonscg_mfra/cartridge/client/default/js/config/slickConfigs.js
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/config/slickConfigs.js
@@ -24,7 +24,23 @@ module.exports = {
         dots: false,
         arrows: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     },
 
     pdp: {
