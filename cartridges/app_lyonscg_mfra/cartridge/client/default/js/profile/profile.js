5859886502aaffd06573f6491863b2d5fc284f7f
Bob Humphrey <bhumphrey@lyonscg.com>
2019-09-04 14:45:43 +0000

    Merged in feature/LRA-231-extend-profile-client-js (pull request #242)
    LRA-231 #comment Removing duplicate methods in lyons version of profile.js and extending from base to get confirm email input to show / hide as expected;
    
    Approved-by: Dave Haas <dhaas@lyonscg.com>

--- cartridges/app_lyonscg_mfra/cartridge/client/default/js/profile/profile.js
+++ cartridges/app_lyonscg_mfra/cartridge/client/default/js/profile/profile.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var formValidation = require('../components/formValidation');
+var formValidation = require('base/components/formValidation');
+var baseProfile = require('base/profile/profile');
 
-module.exports = {
+module.exports = $.extend(baseProfile, {
     submitProfile: function () {
         $('form.edit-profile-form').submit(function (e) {
             var $form = $(this);
@@ -40,36 +41,6 @@ module.exports = {
             $('#confirmEmail').val('');
             $('.form-control-label[for="confirmEmail"]').removeClass('d-none');
         });
-    },
-
-    submitPassword: function () {
-        $('form.change-password-form').submit(function (e) {
-            var $form = $(this);
-            e.preventDefault();
-            var url = $form.attr('action');
-            $form.spinner().start();
-            $('form.change-password-form').trigger('password:edit', e);
-            $.ajax({
-                url: url,
-                type: 'post',
-                dataType: 'json',
-                data: $form.serialize(),
-                success: function (data) {
-                    $form.spinner().stop();
-                    if (!data.success) {
-                        formValidation($form, data);
-                    } else {
-                        location.href = data.redirectUrl;
-                    }
-                },
-                error: function (err) {
-                    if (err.responseJSON.redirectUrl) {
-                        window.location.href = err.responseJSON.redirectUrl;
-                    }
-                    $form.spinner().stop();
-                }
-            });
-            return false;
-        });
     }
-};
+});
+
