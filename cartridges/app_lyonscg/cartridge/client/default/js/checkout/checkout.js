5698da204bc4b6c5e433d83462facbd9a1c160b0
Chris Connell <cconnell@lyonscg.com>
2019-11-21 22:00:13 +0000

    Merged in feature/LRA-242-add-shipping-surcharge (pull request #257)
    LRA-242 Add ShippingSurcharge value to checkout
    
    Approved-by: Dave Haas <dhaas@lyonscg.com>

--- cartridges/app_lyonscg/cartridge/client/default/js/checkout/checkout.js
+++ cartridges/app_lyonscg/cartridge/client/default/js/checkout/checkout.js
@@ -3,7 +3,7 @@
 var addressHelpers = require('base/checkout/address');
 var shippingHelpers = require('base/checkout/shipping');
 var billingHelpers = require('base/checkout/billing');
-var summaryHelpers = require('base/checkout/summary');
+var summaryHelpers = require('lyonscg/checkout/summary');
 var formHelpers = require('base/checkout/formErrors');
 var scrollAnimate = require('base/components/scrollAnimate');
 
