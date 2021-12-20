53792fc335f5a905eaf9fb09347f0b42bb2b4e27
Stas <szhalba@sharpminds.com>
2021-12-20 10:40:43 +0200

    Task 2: Decorate and includes in ISML templates

--- cartridges/site_staszhalba/cartridge/controllers/Training.js
+++ cartridges/site_staszhalba/cartridge/controllers/Training.js
@@ -16,4 +16,28 @@ server.get('IsmlTags', function (req, res, next) {
     next();
 });
 
+server.get('Decorate', function (req, res, next) {
+    var viewData = {
+        booleanFlag: !!req.querystring.booleanFlag,
+        arrayToIterate: ['Apple', 'Samsung', 'Xiaomi'],
+        objectToIterate: {
+            name: 'John',
+            surname: 'Doe',
+            phone: 1111111111
+        }
+    };
+
+    res.render('decorate', viewData);
+    next();
+});
+
+server.get('SimpleRequest', function (req, res, next) {
+    var queryMyParam = req.querystring.myParam;
+
+    res.render('simple-request', {
+        queryMyParam: queryMyParam
+    });
+    next();
+});
+
 module.exports = server.exports();
