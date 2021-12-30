e5b3ec9ccdecceba1a2a6b45753b39b694d618a0
Stas <szhalba@sharpminds.com>
2021-12-30 14:39:06 +0200

    Added the isSent condition

--- cartridges/org_staszhalba/cartridge/scripts/steps/newsletterSendEmail.js
+++ cartridges/org_staszhalba/cartridge/scripts/steps/newsletterSendEmail.js
@@ -3,13 +3,15 @@
 module.exports = {
     execute: function (params) {
         var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+        var Transaction = require('dw/system/Transaction');
         var Mail = require('dw/net/Mail');
 
         var NEWSLETTER_CUSTOM_OBJ = 'NewsletterSubscriptionStasZhalba';
 
         var queryNewsletterCreationDate = new Date(Date.now() - (60 * 60 * 1000 * params.Hours));
+        var queryNewsletter = 'creationDate > {0} AND custom.isSent != {1}';
 
-        var newsletters = CustomObjectMgr.queryCustomObjects(NEWSLETTER_CUSTOM_OBJ, 'creationDate > {0}', null, queryNewsletterCreationDate);
+        var newsletters = CustomObjectMgr.queryCustomObjects(NEWSLETTER_CUSTOM_OBJ, queryNewsletter, null, queryNewsletterCreationDate, true);
         var newslettersArray = newsletters.asList().toArray();
 
         newslettersArray.forEach(function (newsletterObj) {
@@ -19,6 +21,15 @@ module.exports = {
             sendEmail.setFrom('szhalba@speroteck.com');
             sendEmail.setContent('Simple email text');
             sendEmail.send();
+
+            Transaction.wrap(function () {
+                var newsletterCustomObj = CustomObjectMgr.getCustomObject(NEWSLETTER_CUSTOM_OBJ, newsletterObj.custom.email);
+                if (newsletterCustomObj === null) {
+                    return;
+                }
+
+                newsletterCustomObj.custom.isSent = true;
+            });
         });
     }
 };
