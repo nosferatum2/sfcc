647a747abd9e698c0626469a1bac8d8aa7fa59c2
Artur Voshchanikin <avoshchanikin@speroteck.com>
2022-10-03 14:10:02 +0300

    Rewritten to a getAtsValue func.

--- cartridges/site_avoshchanikin/cartridge/models/product/decorators/availability.js
+++ cartridges/site_avoshchanikin/cartridge/models/product/decorators/availability.js
@@ -4,31 +4,37 @@ var Resource = require('dw/web/Resource');
 
 var base = module.superModule;
 
+/**
+ * Creates an object of the visible attributes for a product
+ * @param {dw.catalog.Product} availabilityModel - availabilityModel for a given product.
+ * @return {Object} an object containing the visible attribute 'ats' for a product.
+ */
+function getAtsValue(availabilityModel) {
+    var ATS = {};
+    ATS.messages = [];
+    var inventoryRecord = availabilityModel.inventoryRecord;
+
+    if (inventoryRecord) {
+        ATS.messages.push(
+            Resource.msgf(
+                'label.quantity.in.stock',
+                'common',
+                null,
+                inventoryRecord.ATS.value
+            )
+        );
+    }
+
+    return ATS;
+}
 
 module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
     base.call(this, object, quantity, minOrderQuantity, availabilityModel);
 
+
     Object.defineProperty(object, 'ats', {
         enumerable: true,
-        // value: getATSMessage(availabilityModel)
-        value: (function () {
-            var ATS = {};
-            ATS.messages = [];
-            var inventoryRecord = availabilityModel.inventoryRecord;
-
-            if (inventoryRecord) {
-                ATS.messages.push(
-                    Resource.msgf(
-                        'label.quantity.in.stock',
-                        'common',
-                        null,
-                        inventoryRecord.ATS.value
-                    )
-                );
-            }
-
-            return ATS;
-        }())
+        value: getAtsValue(availabilityModel)
     });
 };
 
