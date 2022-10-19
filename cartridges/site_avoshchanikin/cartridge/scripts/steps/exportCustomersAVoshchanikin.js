a02d901b8411a72d24f860ef302eace6f1ba868e
Artur Voshchanikin <avoshchanikin@speroteck.com>
2022-10-19 21:29:20 +0300

    Added email validation. Fixed annotations.

--- cartridges/site_avoshchanikin/cartridge/scripts/steps/exportCustomersAVoshchanikin.js
+++ cartridges/site_avoshchanikin/cartridge/scripts/steps/exportCustomersAVoshchanikin.js
@@ -5,6 +5,9 @@ var iterator = require('dw/util/Iterator');
 var File = require('dw/io/File');
 var FileWriter = require('dw/io/FileWriter');
 var CSVStreamWriter = require('dw/io/CSVStreamWriter');
+var Site = require('dw/system/Site');
+var URLUtils = require('dw/web/URLUtils');
+var Logger = require('dw/system/Logger');
 
 /**
  * @function exportCustomers
@@ -14,16 +17,16 @@ var CSVStreamWriter = require('dw/io/CSVStreamWriter');
  */
 module.exports = {
     exportCustomers: function (parameters) {
-        var Site = require('dw/system/Site');
         var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
         var currentTime = new Date().toLocaleDateString();
+        var WEBDAV_SERVLET = '/on/demandware.servlet/webdav/Sites';
         var fileName = currentTime + '_customersAVoshchanikin.csv';
         var csvFile;
         var fileWriter;
         var csvStreamWriter;
         var csvURL;
 
-        csvFile = new File('/IMPEX/src/CustomObjectExport/' + fileName);
+        csvFile = new File(File.IMPEX + File.SEPARATOR + 'src' + File.SEPARATOR + 'CustomObjectExport' + File.SEPARATOR + fileName);
         fileWriter = new FileWriter(csvFile);
         csvStreamWriter = new CSVStreamWriter(fileWriter);
 
@@ -34,11 +37,12 @@ module.exports = {
             'Creation Date']);
 
         iterator = CustomerMgr.searchProfiles(
-            'creationDate >= {0} AND creationDate < {1} AND custom.isCustomer_avoshchanikin = true',
+            '(creationDate >= {0} AND creationDate <= {1}) AND custom.isCustomer_avoshchanikin = {2}',
             'creationDate',
             parameters.startDate,
-            parameters.endDate
-            );
+            parameters.endDate,
+            'true'
+        );
 
         while (iterator.hasNext()) {
             var customer = iterator.next();
@@ -51,10 +55,23 @@ module.exports = {
         }
         csvStreamWriter.close();
 
-        csvURL = 'https://zzrj-049.sandbox.us03.dx.commercecloud.salesforce.com/on/demandware.servlet/webdav/Sites' + csvFile.getFullPath();
+        var rootPathcsvFile = WEBDAV_SERVLET + csvFile.fullPath;
+        csvURL = URLUtils.https(rootPathcsvFile).toString().split('/on')[0] + rootPathcsvFile;
+
+        // Checkes emails from parameter string
+        var emails = [];
+        var emailsArr = parameters.email.replace(/\s/, '').split(',');
+        emailsArr.forEach(function (email) {
+            if (emailHelpers.validateEmail(email)) {
+                emails.push(email);
+            } else {
+                Logger.warn("The email address isn't in the correct format - " + email);
+            }
+        });
+        emails.join(', ').toString();
 
         var emailObj = {
-            to: parameters.email,
+            to: emails,
             subject: 'Daily csv file',
             from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@salesforce.com'
         };
