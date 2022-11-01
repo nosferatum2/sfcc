1612d37496bce521c7f8f6886c7df4f8f9a4fa3f
Artur Voshchanikin <avoshchanikin@speroteck.com>
2022-11-01 22:38:06 +0200

    Added full data model in beforePATCH hook.

--- cartridges/site_avoshchanikin/cartridge/scripts/hooks/orderMailSend.js
+++ cartridges/site_avoshchanikin/cartridge/scripts/hooks/orderMailSend.js
@@ -1,32 +1,56 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
+
 'use strict';
 
-var OrderMgr = require('dw/order/OrderMgr');
 var Site = require('dw/system/Site');
+var Status = require('dw/system/Status');
 var Logger = require('dw/system/Logger');
 
 function beforePatchSendMail(basket) {
     var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers');
-    var iterator = require('dw/util/Iterator');
+    var collections = require('*/cartridge/scripts/util/collections');
 
     var template = 'order/sendMailHook';
     var emailObj;
     var context = {};
-    // var order;
 
-    // Logger.info(basket);
+    var productItems = [];
+    collections.forEach(basket.productLineItems, function (productLineItem) {
+        var productFirstImage = productLineItem.getProduct().getImage('small');
+        productItems.push({
+            id: productLineItem.product.ID,
+            productName: productLineItem.productName,
+            quantity: productLineItem.quantity.value,
+            price: productLineItem.price.value,
+            img: {
+                alt: productFirstImage.alt,
+                url: productFirstImage.getHttpsURL()
+            }
+        });
+    });
+
+    context.creationDate = basket.creationDate;
+    context.total = basket.getTotalGrossPrice().value;
+    context.totalShiping = basket.getShippingTotalPrice().value;
+    context.totalTax = basket.getTotalTax().value;
+    context.totalProducts = productItems.length;
+    context.productsData = productItems;
+
+
+    Logger.getLogger('avoshchanikin_scope').info(JSON.stringify(context));
+
     emailObj = {
-        to: 'avoshchanikin@speroteck.com',
-        subject: 'test',
+        to: basket.customerEmail,
+        subject: 'from avoshchanikin scope',
         from: Site.current.getCustomPreferenceValue('customerServiceEmail') || 'no-reply@salesforce.com'
     };
-
-    context.data = 'basket';
 
     emailHelpers.send(emailObj, template, {
         context: context
     });
+
+    return new Status(Status.OK);
 }
 
 exports.beforePATCH = beforePatchSendMail;
